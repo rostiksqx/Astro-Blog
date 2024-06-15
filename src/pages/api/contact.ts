03e810f6718d53f10f0cedb228a0467c9bf7277c
sinarhen-knu <105736826+sinarhen@users.noreply.github.com>
2024-06-15 17:51:53 +0300

    Email sending for contact form is now working

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,33 @@
+import type { APIRoute } from "astro";
+import { Resend } from 'resend';
+import { contactUsHeaderId, errorSearchParameterName } from "../../constants";
+
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    try {
+        const data = await request.formData();
+        const name = data.get("name");
+        const email = data.get("email");
+        const message = data.get("message");
+    
+        if (!name || !email || !message) {
+            return redirect(`/?${errorSearchParameterName}=All fields are required.#${contactUsHeaderId}`)
+        }
+    
+        const apiKey = import.meta.env.EMAIL_SERVICE_API_KEY;
+        const resend = new Resend(apiKey);
+    
+        resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email.toString(),
+            subject: 'Thank you for choosing our coffee shop!',
+            html: '<p>Bro you are real <strong>sigma</strong>!</p>'
+        });
+
+        return redirect("/thank-you");
+    } catch (error){
+        console.error("[ERROR][POST]Received error at server side ", error)
+        return redirect(`/?${errorSearchParameterName}=Something went wrong#${contactUsHeaderId}`);
+    }
+    
+};

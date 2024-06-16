30f536ca607a5dbca0b2dfaaf5a3d5d09986b297
Rostyslav <rostikboss2005@gmail.com>
2024-06-16 13:46:14 +0300

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind()],
+  integrations: [react(), markdoc(), keystatic(), tailwind(), sitemap()],
   output: 'server',
+  site: "https://astro-coffee.onrender.com",
   adapter: node({
     mode: "standalone"
   })
